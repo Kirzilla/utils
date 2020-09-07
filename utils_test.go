43457f684e69cf4b9b972f1b894dfc71872e5fe8
Kirill Beloshitskiy <k.beloshitckii@petrovich.ru>
2020-09-07 15:04:51 +0300

    added ContainsInt

--- utils_test.go
+++ utils_test.go
@@ -13,12 +13,34 @@ func TestContains(t *testing.T) {
 		want bool
 	}{
 		{name: "contains", args: args{a: []string{"a", "b", "c"}, x: "a"}, want: true},
-		{name: "not contains", args: args{a: []string{"a", "b", "c"}, x: "a"}, want: true},
+		{name: "not contains", args: args{a: []string{"a", "b", "c"}, x: "x"}, want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Contains(tt.args.a, tt.args.x); got != tt.want {
 				t.Errorf("Contains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestContainsInt(t *testing.T) {
+	type args struct {
+		a []int
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "contains", args: args{a: []int{1, 2, 3}, x: 1}, want: true},
+		{name: "not contains", args: args{a: []int{1, 2, 3}, x: 10}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ContainsInt(tt.args.a, tt.args.x); got != tt.want {
+				t.Errorf("ContainsInt() = %v, want %v", got, tt.want)
 			}
 		})
 	}
